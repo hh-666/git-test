5dc76d568f1ee9a7917777b0cc45e0c174195cc0
哇哈哈 <7583150+whoaha_3l@user.noreply.gitee.com>
2022-09-20 20:54:25 +0800

    merge success

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -3,11 +3,12 @@ package com.atiguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea git");
-        System.out.println("hello idea git2");
+        System.out.println("这里改一下");
 
-        System.out.println("不行啊");
+        System.out.println("我觉得可以不用改");
         System.out.println("master git4");
         System.out.println("push test git4");
         System.out.println("pull test git4");
+        System.out.println("conflict test");
     }
 }
