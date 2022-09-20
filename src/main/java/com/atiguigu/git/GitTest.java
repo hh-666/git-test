9138a20fd5ba5bc3c1eccb51d67e81808a4f9ca0
hh-666 <55239481+hh-666@users.noreply.github.com>
2022-09-20 20:56:15 +0800

    again modified

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -3,9 +3,9 @@ package com.atiguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea git");
-        System.out.println("这里改一下");
+ 
 
-        System.out.println("我觉得可以不用改");
+        System.out.println("要改的要改的");
         System.out.println("master git4");
         System.out.println("push test git4");
         System.out.println("pull test git4");
