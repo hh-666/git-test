5b66435e72749ebd1579ce450ef570fcab25e97b
hh-666 <55239481+hh-666@users.noreply.github.com>
2022-09-20 20:47:17 +0800

    update four

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("hello idea git");
         System.out.println("hello idea git2");
 
-        System.out.println("hot-fix git3");
+        System.out.println("不行啊");
         System.out.println("master git4");
         System.out.println("push test git4");
         System.out.println("pull test git4");
