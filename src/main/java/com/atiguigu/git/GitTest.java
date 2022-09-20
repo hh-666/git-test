4d3b0536660cfb679ff4d593429a4f3b6c7b3984
哇哈哈 <7583150+whoaha_3l@user.noreply.gitee.com>
2022-09-20 20:08:11 +0800

    hot-fix commit

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello idea git");
         System.out.println("hello idea git2");
+        System.out.println("hello idea git2");
     }
 }
